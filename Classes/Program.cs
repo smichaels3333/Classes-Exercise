d4d94cbb059e41e29ca226566ae3f4de6c663f58
Stephen Michaels <stephen.michaels3333@gmail.com>
2023-01-14 11:00:06 -0500

    initial commit

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var myCar = new Car();
+            myCar.Make = "Ford";
+            myCar.Model = "Focus";
+            myCar.Year = 2013;
+
+            var chevy = new Car()
+            {
+                Make = "Chevy",
+                Model = "Silverado",
+                Year = 2015
+
+
+            };
+
+            var toyota = new Car("Toyota", "Tundra", 2016);
+
+            var carList = new List<Car>() { myCar, chevy, toyota};
+            foreach(var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
         }
     }
 }
